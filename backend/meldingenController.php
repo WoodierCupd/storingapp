70c444e1981ffa3cd825fdbb676ac3c64ea217f1
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2021-03-08 14:25:00 +0100

    Update for the backend
    now you can send stuff to the backend

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -11,7 +11,13 @@ echo $attractie . " / " . $capaciteit . " / " . $melder;
 require_once 'conn.php';
 
 //2. Query
-
+$query = "INSERT INTO meldingen (attractie, capaciteit, melder) VALUES (:attractie, :capaciteit, :melder)";
 //3. Prepare
-
+$statment = $conn->prepare($query);
 //4. Execute
+$statment->execute([
+    "attractie" => $attractie,
+    "capaciteit" => $capaciteit,
+    "melder" => $melder
+]);
+header("Location: ../meldingen/index.php?msg=Melding Opgeslagen");
